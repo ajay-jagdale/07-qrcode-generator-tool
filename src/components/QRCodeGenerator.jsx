bfbb8fadc7b0286503e893a242e2883aa875ed31
ajay-jagdale <ajayjagdale2912@gmail.com>
2024-08-31 15:14:13 +0530

    change

--- src/components/QRCodeGenerator.jsx
+++ src/components/QRCodeGenerator.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { QRCode } from 'qrcode.react';
+import { QRCode } from 'react-qr-code';
 
 const QRCodeGenerator = () => {
     const [inputText, setInputText] = useState('');
